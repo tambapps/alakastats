1ee075cce86ce9fba2f6a7a5ce79fd82abc564a3
Nelson Fonkoua <tambapps@gmail.com>
2025-09-17 20:37:26 +0200

    made GameOutputCard of square size

--- composeApp/src/commonMain/kotlin/com/tambapps/pokemon/alakastats/ui/composables/Buttons.kt
+++ composeApp/src/commonMain/kotlin/com/tambapps/pokemon/alakastats/ui/composables/Buttons.kt
@@ -2,7 +2,9 @@ package com.tambapps.pokemon.alakastats.ui.composables
 
 import alakastats.composeapp.generated.resources.Res
 import alakastats.composeapp.generated.resources.arrow_back
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -14,6 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.navigator.Navigator
@@ -47,14 +50,16 @@ fun GameOutputCard(output: GameOutput) {
         GameOutput.UNKNOWN -> UNKNOWN_COLOR
     }
     Card(
+        modifier = Modifier.size(42.dp),
         shape = RoundedCornerShape(12.dp),
         colors = CardDefaults.cardColors(
             containerColor = color
         ),
     ) {
         Text(
-            modifier = Modifier.padding(all = 8.dp),
+            modifier = Modifier.padding(all = 8.dp).fillMaxSize(),
             text = output.name[0].toString(),
+            textAlign = TextAlign.Center,
             style = MaterialTheme.typography.bodyLarge,
             fontWeight = FontWeight.Bold,
             fontSize = 22.sp
