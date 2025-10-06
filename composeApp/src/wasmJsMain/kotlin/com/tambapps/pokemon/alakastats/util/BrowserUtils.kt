2eb0872ad5f81d94a210717934c7d65435615b1e
Nelson Fonkoua <tambapps@gmail.com>
2025-10-06 17:14:33 +0200

    fixed url resolving for images

--- composeApp/src/wasmJsMain/kotlin/com/tambapps/pokemon/alakastats/util/BrowserUtils.kt
+++ composeApp/src/wasmJsMain/kotlin/com/tambapps/pokemon/alakastats/util/BrowserUtils.kt
@@ -3,6 +3,10 @@ package com.tambapps.pokemon.alakastats.util
 import kotlinx.browser.window
 
 fun getCurrentBaseUrl(): String {
-    val location = window.location
-    return "${location.protocol}//${location.host}"
+    val base = window.location.href.substringBeforeLast('/')
+    return if (base.endsWith("index.html")) {
+        base.substringBeforeLast('/')
+    } else {
+        base
+    }
 }
