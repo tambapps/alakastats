83ab5fa265f6d706fe8ab8c516f556d7907b0d34
Nelson Fonkoua <tambapps@gmail.com>
2025-10-09 22:10:03 +0200

    made background fit full screen

--- composeApp/src/commonMain/kotlin/com/tambapps/pokemon/alakastats/ui/screen/home/HomeScreenMobile.kt
+++ composeApp/src/commonMain/kotlin/com/tambapps/pokemon/alakastats/ui/screen/home/HomeScreenMobile.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -31,36 +32,40 @@ import org.jetbrains.compose.resources.painterResource
 @Composable
 internal fun HomeScreenMobile(isDarkTheme: Boolean, viewModel: HomeViewModel) {
     Column(
-        modifier = Modifier.fillMaxWidth()
+        Modifier.fillMaxSize()
             .background(MaterialTheme.colorScheme.background)
-            .safeContentPadding()
-            .verticalScroll(rememberScrollState()),
-        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Image(
-            painter = painterResource(if (isDarkTheme) Res.drawable.alakastats_dark else Res.drawable.alakastats),
-            contentDescription = "Alakastats logo",
-            modifier = Modifier.size(80.dp),
-            contentScale = ContentScale.Fit
-        )
-        Text("Alakastats", style = MaterialTheme.typography.displayLarge, fontWeight = FontWeight.Bold, textAlign = TextAlign.Center)
-        CatchPhrase(skipLine = true, textAlign = TextAlign.Center)
+        Column(
+            modifier = Modifier.fillMaxWidth()
+                .safeContentPadding()
+                .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(if (isDarkTheme) Res.drawable.alakastats_dark else Res.drawable.alakastats),
+                contentDescription = "Alakastats logo",
+                modifier = Modifier.size(80.dp),
+                contentScale = ContentScale.Fit
+            )
+            Text("Alakastats", style = MaterialTheme.typography.displayLarge, fontWeight = FontWeight.Bold, textAlign = TextAlign.Center)
+            CatchPhrase(skipLine = true, textAlign = TextAlign.Center)
 
-        Spacer(Modifier.height(16.dp))
+            Spacer(Modifier.height(16.dp))
 
-        NewTeamButton(modifier = Modifier.fillMaxWidth().padding(horizontal = 8.dp))
-        Spacer(Modifier.height(4.dp))
-        Row(modifier = Modifier.fillMaxWidth().padding(horizontal = 8.dp)) {
-            ImportTeamButton(viewModel, Modifier.weight(1f))
-            Spacer(Modifier.width(8.dp))
-            SampleTeamButton(viewModel, Modifier.weight(1f))
+            NewTeamButton(modifier = Modifier.fillMaxWidth().padding(horizontal = 8.dp))
+            Spacer(Modifier.height(4.dp))
+            Row(modifier = Modifier.fillMaxWidth().padding(horizontal = 8.dp)) {
+                ImportTeamButton(viewModel, Modifier.weight(1f))
+                Spacer(Modifier.width(8.dp))
+                SampleTeamButton(viewModel, Modifier.weight(1f))
+            }
+
+            Spacer(Modifier.height(16.dp))
+
+            for (team in viewModel.teamlyticsList) {
+                TeamCard(viewModel, team, Modifier.padding(all = 8.dp))
+            }
+            Spacer(Modifier.height(16.dp))
         }
-
-        Spacer(Modifier.height(16.dp))
-
-        for (team in viewModel.teamlyticsList) {
-            TeamCard(viewModel, team, Modifier.padding(all = 8.dp))
-        }
-        Spacer(Modifier.height(16.dp))
     }
 }
