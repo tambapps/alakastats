77720be208c847ecfe8d40678af551db4c651a9b
Nelson Fonkoua <tambapps@gmail.com>
2025-10-09 20:21:46 +0200

    a little more space

--- composeApp/src/commonMain/kotlin/com/tambapps/pokemon/alakastats/ui/composables/StatCard.kt
+++ composeApp/src/commonMain/kotlin/com/tambapps/pokemon/alakastats/ui/composables/StatCard.kt
@@ -4,7 +4,9 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -97,5 +99,6 @@ internal fun <T> DesktopStatCard(
                 rowContent.invoke(this, it)
             }
         }
+        Spacer(Modifier.height(8.dp))
     }
 }
