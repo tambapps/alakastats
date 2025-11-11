bb9b3c2515c43322f619f58572ac409615776e6a
Nelson Fonkoua <tambapps@gmail.com>
2025-11-11 01:00:03 +0100

    removed unused code

--- composeApp/src/commonMain/kotlin/com/tambapps/pokemon/alakastats/ui/screen/teamlytics/lead/LeadStatsTab.kt
+++ composeApp/src/commonMain/kotlin/com/tambapps/pokemon/alakastats/ui/screen/teamlytics/lead/LeadStatsTab.kt
@@ -33,12 +33,9 @@ import com.tambapps.pokemon.PokemonName
 import com.tambapps.pokemon.alakastats.ui.composables.FabLayout
 import com.tambapps.pokemon.alakastats.ui.composables.PokemonCard
 import com.tambapps.pokemon.alakastats.ui.composables.ScrollableRow
-import com.tambapps.pokemon.alakastats.ui.composables.StatCard
 import com.tambapps.pokemon.alakastats.ui.screen.teamlytics.FiltersButton
 import com.tambapps.pokemon.alakastats.ui.service.FacingDirection
 import com.tambapps.pokemon.alakastats.ui.theme.LocalIsCompact
-import com.tambapps.pokemon.alakastats.ui.theme.statCardPercentageWidth
-import com.tambapps.pokemon.alakastats.ui.theme.statCardPokemonSpriteSize
 import kotlin.math.pow
 
 @Composable
@@ -273,71 +270,4 @@ private fun NoData(viewModel: LeadStatsViewModel) {
     Box(Modifier.fillMaxSize()) {
         Text(if (!viewModel.useCase.hasFilteredReplays) "No data" else "No replays matched the filters", style = MaterialTheme.typography.titleSmall, modifier = Modifier.align(Alignment.Center))
     }
-}
-
-// TODO delete below
-@Composable
-internal fun MostCommonLeadCard(viewModel: LeadStatsViewModel, modifier: Modifier = Modifier) {
-    LeadCard(
-        viewModel = viewModel,
-        data = viewModel.duoStatsMap.entries.map { it.key to it.value }.sortedBy { (_, stats) -> - stats.total },
-        title = "Most Common Lead",
-        modifier = modifier,
-    )
-}
-
-@Composable
-internal fun MostEffectiveLeadCard(viewModel: LeadStatsViewModel, modifier: Modifier = Modifier) {
-    LeadCard(
-        viewModel = viewModel,
-        data = viewModel.duoStatsMap.entries.map { it.key to it.value }.sortedBy { (_, stats) -> - stats.winRate },
-        title = "Most Effective Lead",
-        modifier = modifier,
-    )
-}
-
-@Composable
-internal fun LeadAndWin(viewModel: LeadStatsViewModel, modifier: Modifier = Modifier) {
-    LeadCard(
-        viewModel = viewModel,
-        data = viewModel.pokemonStats.entries.map { listOf(it.key) to it.value }.sortedBy { (_, stats) -> - stats.winRate },
-        title = "Lead And Win",
-        modifier = modifier,
-    )
-}
-
-@Composable
-private fun LeadCard(
-    viewModel: LeadStatsViewModel,
-    data: List<Pair<List<PokemonName>, WinStats>>,
-    title: String,
-    modifier: Modifier) {
-    StatCard(
-        title = title,
-        modifier = modifier,
-        data = data,
-    ) { (lead, stats) ->
-        Spacer(Modifier.width(8.dp))
-        lead.forEach { pokemon ->
-            viewModel.pokemonImageService.PokemonSprite(pokemon, modifier = Modifier.size(statCardPokemonSpriteSize))
-        }
-        Text(
-            text = if (stats.total == 0) "Did not lead"
-            else "Won\n${stats.winCount} out of ${stats.total} games",
-            modifier = Modifier.weight(1f),
-            textAlign = TextAlign.Center
-        )
-        PercentageText(stats.winRate)
-        Spacer(Modifier.width(8.dp))
-    }
-}
-
-@Composable
-fun PercentageText(rate: Float) {
-    Text(
-        "${rate.times(100).toInt()}%",
-        style = MaterialTheme.typography.titleLarge,
-        modifier = Modifier.width(statCardPercentageWidth),
-        textAlign = TextAlign.Center,
-    )
 }
