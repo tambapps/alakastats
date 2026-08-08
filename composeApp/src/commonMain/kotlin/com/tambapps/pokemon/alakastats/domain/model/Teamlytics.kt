08d4635efaf5fb7b269b6b6d555a91db4854e6bb
Nelson Fonkoua <tambapps@gmail.com>
2026-08-08 18:44:14 +0200

    order matters

--- composeApp/src/commonMain/kotlin/com/tambapps/pokemon/alakastats/domain/model/Teamlytics.kt
+++ composeApp/src/commonMain/kotlin/com/tambapps/pokemon/alakastats/domain/model/Teamlytics.kt
@@ -15,8 +15,8 @@ enum class Format(
     val pokemonLevel: Int? = null
 ) {
     NONE("<none>", Mechanic.entries),
-    REGULATION_MA("Regulation M-A", allowedMechanics = listOf(Mechanic.MEGA_EVOLUTION), pokemonLevel = 50),
     REGULATION_MB("Regulation M-B", allowedMechanics = listOf(Mechanic.MEGA_EVOLUTION), pokemonLevel = 50),
+    REGULATION_MA("Regulation M-A", allowedMechanics = listOf(Mechanic.MEGA_EVOLUTION), pokemonLevel = 50),
     REGULATION_I("Regulation I", allowedMechanics = listOf(Mechanic.TERASTALLIZATION), pokemonLevel = 50),
     REGULATION_F("Regulation F", allowedMechanics = listOf(Mechanic.TERASTALLIZATION), pokemonLevel = 50),
     REGULATION_H("Regulation H", allowedMechanics = listOf(Mechanic.TERASTALLIZATION), pokemonLevel = 50),
